7b7fc91ff1045065cd5267e462b9dc5c6ec2d3de
K M LINGAUDHAYA <lingaudhayakm@gmail.com>
2024-07-12 23:54:52 +0530

    added function getPrevoiusFormData to display the previously filled data in case of for errors

--- assets/pages/signup.php
+++ assets/pages/signup.php
@@ -10,11 +10,14 @@
                 <h1 class="h5 mb-3 fw-normal">Create new account</h1>
                 <div class="d-flex">
                     <div class="form-floating mt-1 col-6 ">
-                        <input required type="text" name="first_name" class="form-control rounded-0" placeholder="username/email">
+                        <input  type="text" name="first_name" value="<?php echo getPreviousFormData('first_name'); ?>" class="form-control rounded-0" placeholder="username/email">
                         <label for="floatingInput">first name</label>
                     </div>
+
+                    
+
                     <div class="form-floating mt-1 col-6">
-                        <input required type="text" name="last_name" class="form-control rounded-0" placeholder="username/email">
+                        <input  type="text" name="last_name" value="<?php echo getPreviousFormData('last_name'); ?>" class="form-control rounded-0" placeholder="username/email">
                         <label for="floatingInput">last name</label>
                     </div>
                 </div>
@@ -42,20 +45,36 @@
                     </div>
                 </div>
                 <div class="form-floating mt-1">
-                    <input required type="email" name="email" class="form-control rounded-0" placeholder="username/email" autocomplete="off">
+                    <input type="email" name="email" class="form-control rounded-0" placeholder="username/email" autocomplete="off">
                     <label for="floatingInput">email</label>
                 </div>
                 <div class="form-floating mt-1">
-                    <input required type="text" name="username" class="form-control rounded-0" placeholder="username/email" autocomplete="off">
+                    <input type="text" name="username" class="form-control rounded-0" placeholder="username/email" autocomplete="off">
                     <label for="floatingInput">username</label>
                 </div>
-                <div class="form-floating mt-1">
-                    <input required type="password" name="password" class="form-control rounded-0" id="floatingPassword" placeholder="Password" autocomplete="off">
-                    <label for="floatingPassword">password</label>
-                </div>
+
+                <?php if(isset($_SESSION['duplicate_error'])): ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $_SESSION['duplicate_error']; ?>
+                    </div>
+                    <?php unset($_SESSION['duplicate_error']); // Don't forget to unset the session variable ?>
+                <?php endif; ?>
                 
 
-                <?php if(isset($_GET['message'])) {echo "<span style='color: red;'>".$_GET['message']."</span>";}?>
+                <div class="form-floating mt-1">
+                    <input type="password" name="password" class="form-control rounded-0" id="floatingPassword" placeholder="Password" autocomplete="off">
+                    <label for="floatingPassword">password</label>
+                </div>
+
+                <?php if(isset($_SESSION['incomplete_error'])): ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $_SESSION['incomplete_error']; ?>
+                    </div>
+                    <?php unset($_SESSION['incomplete_error']); // Don't forget to unset the session variable ?>
+                <?php endif; ?>
+                
+
+                
                 <div class="mt-3 d-flex justify-content-between align-items-center">
                 <input type="hidden" name="form_type" value="signup">
                     <button class="btn btn-primary" type="submit">Sign Up</button>
