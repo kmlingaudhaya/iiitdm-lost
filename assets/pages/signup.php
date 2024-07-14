6d43fef2353a2edfed5a3879d8fdd7b5945021ee
K M LINGAUDHAYA <lingaudhayakm@gmail.com>
2024-07-15 00:07:33 +0530

    nothing to commit

--- assets/pages/signup.php
+++ assets/pages/signup.php
@@ -78,7 +78,7 @@
                 <div class="mt-3 d-flex justify-content-between align-items-center">
                 <input type="hidden" name="form_type" value="signup">
                     <button class="btn btn-primary" type="submit">Sign Up</button>
-                    <a href="#" class="text-decoration-none">Already have an account ?</a>
+                    <a href="?login" class="text-decoration-none">Already have an account ?</a>
 
 
                 </div>
